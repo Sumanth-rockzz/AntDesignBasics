123893c6b0420b0b1c2d7c84eaa65b18ca478425
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-08 02:49:28 +0530

    DatePicker,RangePicker & TimePicker

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { Form, Input, Button, message, Alert } from "antd";
+import { DatePicker } from "antd";
 
 const App = () => {
-  const [showAlert, setShowAlert] = useState(false);
-  const submitHandler = (e) => {
-    console.log(e);
-    setTimeout(() => {
-      setShowAlert(true);
-      //message.success(`Logged In as ${e.username} Successfully`, 5); //[error,warning,success]
-    }, 2000);
-  };
   return (
     <div className="app">
       <header className="app-header">
-        {showAlert && (
-          <Alert
-            type="error"
-            message="error"
-            description="Something went Wrong"
-            closable
-          />
-        )}
-        <Form onFinish={submitHandler}>
-          <Form.Item label="User Name" name="username">
-            <Input placeholder="User Name" required></Input>
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input.Password
-              placeholder="Password"
-              type="password"
-              required
-            ></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+        <DatePicker picker="date" />
+        <DatePicker picker="month" />
+        <DatePicker picker="year" />
+        <DatePicker picker="quarter" />
+        <DatePicker picker="time" />
+        <DatePicker picker="week" />
+        <DatePicker.RangePicker />
+        <DatePicker.RangePicker picker="month" />
+        <DatePicker.TimePicker />
       </header>
     </div>
   );
