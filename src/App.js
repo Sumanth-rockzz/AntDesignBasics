b93eef922880b752153949e26a344bb8c94e799c
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-08 02:59:06 +0530

    Spin Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { DatePicker } from "antd";
+import { Button, Spin } from "antd";
 
 const App = () => {
+  const [spinning, setSpinning] = useState(false);
+  const spinHandler = () => {
+    setSpinning((prev) => !prev);
+  };
   return (
     <div className="app">
       <header className="app-header">
-        <DatePicker picker="date" />
-        <DatePicker picker="month" />
-        <DatePicker picker="year" />
-        <DatePicker picker="quarter" />
-        <DatePicker picker="time" />
-        <DatePicker picker="week" />
-        <DatePicker.RangePicker />
-        <DatePicker.RangePicker picker="month" />
-        <DatePicker.TimePicker />
+        <Spin spinning={spinning} />
+        <Button type="primary" onClick={spinHandler}>
+          Toggle Spinning
+        </Button>
+        <Spin spinning={spinning}>
+          <p>Hello !</p>
+          <p>Hello !</p>
+          <p>Hello !</p>
+        </Spin>
       </header>
     </div>
   );
