7cc38dfda028d348edb6243063cb94cb9480302e
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-08 02:21:56 +0530

    Table Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
 import React from "react";
 import "./App.css";
-import { Form, Input, Button } from "antd";
+import { Table } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
 
 const App = () => {
-  const submitHandler = (e) => {
-    console.log(e);
-  };
+  const data = [
+    {
+      name: "Sumanth",
+      age: 21,
+      city: "Bangalore",
+      state: "Karnataka",
+      key: 1,
+    },
+    {
+      name: "Likith",
+      age: 25,
+      city: "Bangalore",
+      state: "Karnataka",
+      key: 2,
+    },
+    {
+      name: "Charan",
+      age: 17,
+      city: "Bangalore",
+      state: "Karnataka",
+      key: 3,
+    },
+    {
+      name: "Gagan",
+      age: 19,
+      city: "Bangalore",
+      state: "Karnataka",
+      key: 4,
+    },
+  ];
+  let columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "key",
+      render: (value) => {
+        return (
+          <a href="/" onClick={() => console.log(value)}>
+            <UserAddOutlined /> {" " + value}
+          </a>
+        );
+      },
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "key",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "key",
+    },
+    {
+      title: "State",
+      dataIndex: "state",
+      key: "key",
+    },
+    {
+      title: "Eligible to Vote",
+      key: "key",
+      render: (payload) => {
+        return <p>{payload.age >= 18 ? "True" : "False"}</p>;
+      },
+    },
+  ];
   return (
     <div className="app">
       <header className="app-header">
-        <Form onFinish={submitHandler}>
-          <Form.Item label="User Name" name="username">
-            <Input placeholder="User Name" required></Input>
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input.Password
-              placeholder="Password"
-              type="password"
-              required
-            ></Input.Password>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
+        <Table pagination={false} dataSource={data} columns={columns}></Table>
       </header>
     </div>
   );
