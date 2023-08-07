9685d343ac0104ebb8b448b993b5ff3dbec08e09
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-08 00:54:56 +0530

    Button Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+import { Button } from "antd";
+import { PoweroffOutlined } from "@ant-design/icons";
 
 const App = () => {
-  return <div>App</div>;
+  const [loading, setLoading] = useState(false);
+  const onclickHandler = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+  return (
+    <div className="App">
+      <div className="AppHeader">
+        <Button
+          type="primary"
+          on
+          onClick={onclickHandler}
+          loading={loading}
+          icon={<PoweroffOutlined />}
+          className="my-button"
+          style={{ backgroundColor: "yellow" }}
+        >
+          Submit
+        </Button>
+        <Button type="link" href="#">
+          Submit
+        </Button>
+        <Button type="default">Submit</Button>
+        <Button type="dashed" block>
+          Submit
+        </Button>
+        <Button type="text">Submit</Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
