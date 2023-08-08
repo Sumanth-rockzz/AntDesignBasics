7e69c0f529df60e59d508121d61241f6a6012e15
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-08-09 00:58:16 +0530

    Selecting Rows and Custom Selection Options

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import { getToDoList } from "./API/API";
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+  const [alreadySelectedRows, setAlreadySelectedRows] = useState([
+    "1",
+    "2",
+    "3",
+  ]);
   useEffect(() => {
     setLoading(true);
     getToDoList().then((data) => {
+      data = data.map((todo) => {
+        todo.key = todo.id;
+        return todo;
+      });
       setTodoList(data);
       setLoading(false);
+      setTotalCount(data.length);
     });
   }, []);
 
@@ -42,7 +53,10 @@ const App = () => {
       dataIndex: "completed",
       key: "4",
       render: (completed) => {
-        return completed ? "Completed" : "In Progress";
+        const color = completed ? "blue" : "red";
+        return (
+          <Tag color={color}>{completed ? "Completed" : "In Progress"}</Tag>
+        );
       },
       filters: [
         { text: "Complete", value: true },
@@ -60,10 +74,61 @@ const App = () => {
         <Table
           columns={columns}
           dataSource={todoList}
+          rowSelection={{
+            type: "checkbox",
+            onSelect: (data) => {
+              console.log(data);
+            },
+            selectedRowKeys: alreadySelectedRows,
+            onChange: (keys) => {
+              setAlreadySelectedRows(keys);
+            },
+            getCheckboxProps: (data) => ({
+              disabled: data.completed === true,
+            }),
+            hideSelectAll: false, //can be set true
+            selections: [
+              Table.SELECTION_ALL,
+              Table.SELECTION_INVERT,
+              Table.SELECTION_NONE,
+              {
+                key: "even",
+                text: "Select Even Rows",
+                onSelect: (allKeys) => {
+                  const selectedKeys = allKeys.filter((key) => key % 2 === 0);
+                  setAlreadySelectedRows(selectedKeys);
+                },
+              },
+              {
+                key: "completed",
+                text: "Select all with Status Completed",
+                onSelect: (allKeys) => {
+                  const selectedKeys = allKeys.filter((key) => {
+                    return todoList.find((list) => {
+                      return list.key === key && list.status === "completed";
+                    });
+                  });
+                  setAlreadySelectedRows(selectedKeys);
+                },
+              },
+              {
+                key: "progress",
+                text: "Select all with Status In Progress",
+                onSelect: (allKeys) => {
+                  const selectedKeys = allKeys.filter((key) => {
+                    return todoList.find((list) => {
+                      return list.key === key && list.status !== "completed";
+                    });
+                  });
+                  setAlreadySelectedRows(selectedKeys);
+                },
+              },
+            ],
+          }}
           pagination={{
             current: page,
             pageSize: limit,
-            total: 500, //total count of data here
+            total: totalCount, //total count of data here
             onChange: (page, limit) => {
               setPage(page);
               setLimit(limit);
